cdf7162b20bf88a9140a08fd278f3ae5e5d25ee7
unknown <objectifmh@hotmail.com>
2025-01-20 18:08:44 +0100

    feat: installation font awesome pour les icones et footer

--- ADP/src/app/app.component.ts
+++ ADP/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
