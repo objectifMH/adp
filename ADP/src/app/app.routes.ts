ac3c2f97661a16aff90ef47eea5f7454fa9312a4
unknown <objectifmh@hotmail.com>
2025-02-21 13:46:04 +0100

    feat: product list component + creation de product.json

--- ADP/src/app/app.routes.ts
+++ ADP/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ContactComponent } from './features/contact/contact.component';
 import { HomeComponent } from './features/home/home.component';
 import { ProductListComponent } from './features/product-list/product-list.component';
+import { AboutComponent } from './features/about/about.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'products', component: ProductListComponent }, // Product list page
   { path: '404', component: HomeComponent }, // Page dédiée pour les 404 à faire
   { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirection par défaut si aucun chemin n'est fourni
--- ADP/src/app/features/carrousel/carrousel.component.ts
+++ ADP/src/app/features/carrousel/carrousel.component.ts
@@ -19,12 +19,18 @@ export class CarrouselComponent implements OnInit, OnDestroy {
 
   slidesMock = [
     {
-      image: '/banner.jpg',
-      title: 'ADP votre cave en ligne',
-      description:
-        'Alcool rare, bonnes affaires.<br>Découvrez notre sélection exclusive.',
+      image: '/landing5.webp',
+      title: 'Sélection Prestige',
+      description: 'Découvrez nos alcools premium.',
+      ctaText1: 'Explorer',
+      ctaLink1: '/selection',
+    },
+    {
+      image: '/landing6.webp',
+      title: 'Soirée Étudiante',
+      description: "L'ambiance parfaite pour vos événements BDE.",
       ctaText1: 'Découvrir',
-      ctaLink1: '/products',
+      ctaLink1: '/soiree-etudiante',
     },
     {
       image: '/landing1.webp',
@@ -34,7 +40,16 @@ export class CarrouselComponent implements OnInit, OnDestroy {
       ctaLink1: '/whisky',
     },
     {
-      image: '/belvedere.webp',
+      image: '/banner.jpg',
+      title: 'ADP votre cave en ligne',
+      description:
+        'Alcool rare, bonnes affaires.<br>Découvrez notre sélection exclusive.',
+      ctaText1: 'Découvrir',
+      ctaLink1: '/products',
+    },
+
+    {
+      image: '/landing2.webp',
       title: 'Les vodkas Belvedere',
       description:
         'À travers sa collection de vodkas, Belvedere offre une variété de saveurs distinctives, reflets du terroir et de son savoir-faire.',
